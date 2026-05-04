b5f9ef60a09211442022bbe3324fcc0207fdc4b1
Mayuresh Vinayak Kasar <kasarmayuresh444@gmail.com>
2026-05-04 13:49:33 +0530

    ready for deployment

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,17 +1,8 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-// TEST ROUTE
-router.get("/", (req, res) => {
-  res.json({ message: "Auth routes working" });
-});
-
-// REGISTER → /api/auth/register
-router.post("/register", register);
-
-// LOGIN → /api/auth/login
 router.post("/login", login);
 
 export default router;
